fe603864f3fbc9cb0139f997be40b3b45de7ce5f
ismailguzel <ismailgzel@gmail.com>
2025-11-01 03:40:58 +0300

    njobs110

--- 03-models/hierarchical/model2_nonstationary/train_model2.py
+++ 03-models/hierarchical/model2_nonstationary/train_model2.py
@@ -62,7 +62,7 @@ parser.add_argument('--classifier', type=str, default='all',
 args = parser.parse_args()
 
 # Global n_jobs setting for all models
-N_JOBS = -1  # Use all available cores
+N_JOBS = 110  # Use all available cores
 
 print("="*80)
 print("MODEL 2: NON-STATIONARY 5-CLASS CLASSIFICATION")
